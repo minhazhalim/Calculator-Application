964fe6343a06e497c1e71dbd707cb426cfe5748f
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2022-05-09 21:24:35 +0600

    Add files via upload

---
+++ software.js
@@ -0,0 +1,18 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+     button.addEventListener('click',(event) => {
+          if(event.target.innerHTML == '='){
+               string = eval(string);
+               document.querySelector('input').value = string;
+          }
+          else if(event.target.innerHTML == 'C'){
+               string = ""
+               document.querySelector('input').value = string;
+          }
+          else {
+               string = string + event.target.innerHTML;
+               document.querySelector('input').value = string;
+          }
+     });
+});
